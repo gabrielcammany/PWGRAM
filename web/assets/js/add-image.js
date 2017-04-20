5bf0517747bf853340ea3cda53a9dd4c81471367
Manel Manchon <manchon.gasco@gmail.com>
2017-04-20 19:43:17 +0200

    soy idiota

--- web/assets/js/add-image.js
+++ web/assets/js/add-image.js
@@ -3,5 +3,45 @@
  */
 $('#addImage').click(function (e) {
    e.preventDefault();
-   console.log("Holaa");
+   console.log($('#add_image').val());
+   console.log($('#titleImage').val());
+   console.log($('#private').val());
+   console.log($('#public').val());
+   var object={};
+   object.image=$('#add_image').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/upload',
+        data: {myData:$('#add_image').attr('src'),$('#titleImage').val(),},
+        success: function ($response) {
+            console.log('**'+$response);
+        }
+    });
+
 });
+
+function readURL(input) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.readAsDataURL(input.files[0]);
+        reader.onload = function (e) {
+            $('#add_image').attr('src', e.target.result);
+           /*
+            Enviamos la imagen desde el cliente al servidor con un nombre provisional y solo cambiaremos el nombre
+            al registrar al usuario.
+            */
+            $.ajax({
+                type: 'POST',
+                url: '/upload',
+                data: {myData:$('#add_image').attr('src')},
+                success: function ($response) {
+                    console.log('**'+$response);
+                }
+            });
+        }
+
+    }
+}
